ba3c90269bb8ae1ef7c4b56d37081e4af85c5c3f
Tomer <tomerpar133@gmail.com>
2016-01-31 21:06:47 +0200

    inital commit - good luck

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Jan 31, 2016
+ *      Author: tomer
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "yay" << endl;
+	return 0;
+}
+
